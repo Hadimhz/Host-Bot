b2715f4daafea01fc34274feb290608fa46175d7
DarkInk <rstonehouse66@gmail.com>
2021-07-20 19:44:57 -0500

    Adding some comments and Updating config

--- index.js
+++ index.js
@@ -6,15 +6,15 @@
 /_____/\__,_/_/ /_/_____/\____/\__/  /_/ /_/\____/____/\__/_/_/ /_/\__, /
 Free Hosting forever!                                            /____/
 */
-global.config = require("./config.json");
+global.config = require("./config.json"); // Edit example-config.json
 
-global.Discord = require("discord.js");
+global.Discord = require("discord.js"); 
 global.client = new Discord.Client({
-    restTimeOffset: 0,
-    disableMentions: 'everyone',
+    //I've given it all intents Remove any you think it might not need :)
     intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING"],
+    //Made it where it can only ping Users, Stops people from pinging @everyone or @members
     allowedMentions: {
-        parse: ['users', 'roles'],
+        parse: ['users'],
         repliedUser: true
     },
     restWsBridgetimeout: 100,
