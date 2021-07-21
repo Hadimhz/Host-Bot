2457de5daebe7460dcbdac264f0ca73debefaa56
DarkInk <rstonehouse66@gmail.com>
2021-07-20 19:19:37 -0500

    Files :3

---
+++ bot/discord/commands/animalapi.js
@@ -0,0 +1,23 @@
+const superagent = require("snekfetch");
+exports.run = async (client, message, args) => {
+    if (!args[0]) {
+        superagent.get('https://api.danbot.host/total')
+            .end((err, response) => {
+                const embed = new Discord.MessageEmbed()
+                    .addField('Dog API:', response.body.dogtotal + " images. \nhttps://api.danbot.host/dog", true)
+                    .addField('Cat API:', response.body.cattotal + " images. \nhttps://api.danbot.host/cat", true)
+                    .setFooter('You can get a image from the api using DBH!animalapi animalhere')
+                message.channel.send({
+                    embeds: [embed]
+                })
+            })
+    } else if (args[0] === "dog") {
+        superagent.get('https://api.danbot.host/dog')
+            .end((response) => {
+                message.channel.send({
+                    content: "Random dog image from the DanBot Hosting api!",
+                    files: [`${response.body.image}`]
+                });
+            });
+    }
+};
---
+++ bot/discord/commands/exec.js
@@ -0,0 +1,22 @@
+const exec = require('child_process').exec;
+exports.run = (client, message, args) => {
+    if (message.member.roles.cache.find(r => r.id === "778237595477606440")) {
+
+        exec(`${args.join(" ")}`, (error, stdout) => {
+
+            let response = (error || stdout);
+
+            if (response.length > 1024) console.log(response), response = 'Output too long.';
+
+            message.channel.send("", {
+                embeds: [new Discord.MessageEmbed()
+                    .setDescription("```" + response + "```")
+                    .setTimestamp()
+                    .setColor("RANDOM")
+                ]
+            })
+
+        });
+
+    }
+}
