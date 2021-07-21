14b12384abe8a434b00ef761a344661c6f1c1bc4
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-07-22 02:57:02 +0300

    fixes, linting and more readability

--- index.js
+++ index.js
@@ -6,25 +6,28 @@
 /_____/\__,_/_/ /_/_____/\____/\__/  /_/ /_/\____/____/\__/_/_/ /_/\__, /
 Free Hosting forever!                                            /____/
 */
+
 const chalk = require('chalk');
 const fs = require('fs');
+
 global.config = require("./config.json"); // Edit example-config.json
 require("dotenv").config();
-global.Discord = require("discord.js"); 
+global.Discord = require("discord.js");
+
 global.client = new Discord.Client({
     //I've given it all intents Remove any you think it might not need :)
     intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING"],
+
     //Made it where it can only ping Users, Stops people from pinging @everyone or @members
     allowedMentions: {
         parse: ['users'],
         repliedUser: true
     },
-    restWsBridgetimeout: 100,
     partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
-//Bot login
-client.login(process.env.Token);
 
 client.on("ready", () => {
-    console.log('Ready, Logged into as ' + client.user.username)
-})
+    console.log('Ready, Logged in as ' + client.user.tag);
+});
+
+client.login(process.env.Token); // Login to Discord
