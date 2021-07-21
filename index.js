0fec10b1e02c0d5ecd0ac3dd5c054cb963d8dd3c
DarkInk <rstonehouse66@gmail.com>
2021-07-20 20:34:18 -0500

    Removing stuff until Solo adds input validation

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ Free Hosting forever!                                            /____/
 */
 const chalk = require('chalk');
 const fs = require('fs');
-const { loadCommands } = require('./utils/commandHandler');
 global.config = require("./config.json"); // Edit example-config.json
 global.Discord = require("discord.js"); 
 global.client = new Discord.Client({
@@ -21,34 +20,6 @@ global.client = new Discord.Client({
     },
     restWsBridgetimeout: 100,
     partials: ['MESSAGE', 'CHANNEL', 'REACTION']
-});
-
-exports.client = client;
-global.ROOT_PATH = __dirname;
-
-
-// Event Handler
-let events = fs.readdirSync(ROOT_PATH + '/events').filter(x => x.endsWith(".js"));
-events.forEach(x => {
-    try {
-        require('./events/' + x);
-    } catch (error) {
-        console.log(chalk.bgRedBright("[ERROR]"), `An error occured while trying to load the ${x} event`);
-    }
-});
-
-loadCommands(`${ROOT_PATH}/commands`).then(x => {
-    // console.log(x);
-    fs.writeFileSync(ROOT_PATH + '/../log.json', JSON.stringify(x.logs, null, 2));
-    client.commands = x.commandsCol;
-
-    if (x.logs.stats.errors != 0)
-        console.log(chalk.bgRedBright("[ERROR]"), `An error occured while loading commands, please check`, chalk.bgWhite("log.json"), `for more information.`);
-
-    console.log(chalk.bgCyan("[CommandHandler]"), `Loaded a total of ${x.logs.stats.commands} commands in ${x.logs.stats.categories} categories.`);
-})
-client.on('ready', () => {
-    console.log("online");
 });
 //Bot login
 client.login(config.DiscordBot.Token);
