27a5408401d247e4d9bcf53f1eaa15d44f179af2
DarkInk <rstonehouse66@gmail.com>
2021-07-22 03:10:09 -0500

    Last changes

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 const { client } = require('../index.js')
 const config = require('../config.json')
+const exec = require('child_process').exec;
 client.on('ready', async (client) => {
+    console.log(config.DiscordBot.DiscordChannels.gitlogs)
     setInterval(() => {
         exec(`git pull`, (error, stdout) => {
             let response = (error || stdout);
@@ -8,7 +10,7 @@ client.on('ready', async (client) => {
                 if (response.includes("Already up to date.")) {
                     //console.log('Bot already up to date. No changes since last pull')
                 } else {
-                    client.channels.cache.get(config.DiscordChannels.gitChannel).send('**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```' + response + "```" + "\n\n\n**Restarting bot**")
+                    client.channels.cache.get(config.DiscordBot.DiscordChannels.gitlogs).send('**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```' + response + "```" + "\n\n\n**Restarting bot**")
                     setTimeout(() => {
                         process.exit();
                     }, 1000)
