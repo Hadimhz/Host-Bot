2f9c09398231f07ad791ecd4e148e9cc143d27e4
mqrkelich <70708315+mqrkelich@users.noreply.github.com>
2021-07-23 10:42:31 +0200

    Staff Commands

--- commands/Staff/kick.js
+++ commands/Staff/kick.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const config = require("../../config.json")
 module.exports.run = (client, message, args) => {
   
-        if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(`🚧 | You do not have enough permissions to use this command.`)
+if(!message.member.permissions.has('KICK_MEMBERS')) return message.channel.send(`🚧 | You do not have enough permissions to use this command.`)
 
         let user = message.mentions.users.first()
         let member = message.mentions.members.first()
@@ -31,18 +31,11 @@ module.exports.run = (client, message, args) => {
             .setThumbnail(`${message.guild.iconURL({ dynamic:true })}`)
             .setColor(message.guild.me.displayHexColor)
             .setTimestamp()
-            client.channels.cache.get(onfig.DiscordChannels.modLogs).send({embed: [embed]})
+            client.channels.cache.get(config.DiscordChannels.modLogs).send({embed: embed})
         })
 
         .catch((error) => {
-            const embed = new Discord.MessageEmbed()
-            .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
-            .setDescription(`\`\`\`js\n${error}\`\`\``)
-            .setColor(message.guild.me.displayHexColor)
-            .setThumbnail(client.user.avatarURL())
-            .setTimestamp()
-            .setFooter(client.user.username, client.user.avatarURL())
-            message.channel.send({embed: [embed]})
+          message.channel.send(`\`\`\`js\n${error}\`\`\``)
         })
 }
 
