fe5af60d81e966aa465b491e5e9fb73d9b3dc12c
Hadimhz <solo1010.mahfouz@gmail.com>
2021-07-23 04:10:36 +0300

    prevent log spam when mongo fails to connect

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,7 @@ const chalk = require("chalk");
 
 client.on('ready', async (client) => {
     console.log(`${chalk.greenBright("[BOT]")} Bot ready and logged in as ${client.user.tag}`);
-    await mongo().then(() => console.log(chalk.green("[DATABASE]") + " Connected to database successfully!"));
+    await mongo().then(() => console.log(chalk.green("[DATABASE]") + " Connected to database successfully!"))
+        .catch(e => console.error(chalk.bgRedBright("[ERROR]"), `An error has occured when attempting to connect to mongo. (${e.message})`));
 });
+
