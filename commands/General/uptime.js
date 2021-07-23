fc852695b5ba46bc9ea18ff48a00a319aff5f80b
Hadimhz <solo1010.mahfouz@gmail.com>
2021-07-23 08:25:51 +0300

    Update uptime.js

--- commands/General/uptime.js
+++ commands/General/uptime.js
@@ -1,17 +1,21 @@
 const { MessageEmbed } = require("discord.js")
+const humanizeDuration = require("humanize-duration")
 
 
 module.exports.run = async (client, message, args) => {
-    let days = Math.floor(client.uptime / 86400000 );
-    let hours = Math.floor(client.uptime / 3600000 ) % 24;
-    let minutes = Math.floor(client.uptime / 60000) % 60;
-    let seconds = Math.floor(client.uptime / 1000) % 60;
+
     message.channel.send({
-        embeds: [new MessageEmbed().setTitle(`DanBot Hosting Bot Uptime`).setColor("#6f89d9").addField("Uptime:", `${days}d ${hours}h ${minutes}m ${seconds}s`).addField("Memory usage:", Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000) + "mb", true).setFooter(`Requested by ${message.author.tag}`).setTimestamp()]
+        embeds: [
+            new MessageEmbed()
+                .setTitle(`DanBot Hosting Bot Uptime`)
+                .setColor("#6f89d9").addField("Uptime:", humanizeDuration(client.uptime, { round: true }))
+                .addField("Memory usage:", Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000) + "mb", true)
+                .setFooter(`Requested by ${message.author.tag}`).setTimestamp()
+        ]
     });
 }
 
 module.exports.info = {
-    name: 'uptime', 
-    description: "Shows the bot's uptime." 
+    name: 'uptime',
+    description: "Shows the bot's uptime."
 }
