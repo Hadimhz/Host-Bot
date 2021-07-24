48fb5d43945249b5e042a907c3b312adbb5d1196
Hadimhz <solo1010.mahfouz@gmail.com>
2021-07-24 08:54:27 +0300

    [transporter.js] added errors and fixed typo + logs

--- utils/transporter.js
+++ utils/transporter.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer');
 const config = require('../config.json');
+const chalk = require('chalk');
 
 class transporter {
 
     /**
-     * 
      * @param {object} emailData 
      * @param {string} emailData.from 
      * @param {string} emailData.to
@@ -21,7 +21,6 @@ class transporter {
                 pass: config.email.password
             }
         });
-
         this.email = emailData || {};
     }
 
@@ -33,9 +32,18 @@ class transporter {
 
     setText = (txt) => { this.email.text = txt; return this; }
 
-    setHTML = (htlm) => { this.email.html = htlm; return this; }
+    setHTML = (html) => { this.email.html = html; return this; }
 
-    send = async () => await this.transporter.sendMail(this.email);
+    send = async () => {
+
+        if (this.email.to == null) throw new Error("EmailReceiver can't be null.");
+        if (this.email.subject == null) throw new Error("EmailSubject can't be null.");
+        if (this.email.text == null && this.email.html == null) throw new Error("EmailContent can't be null.");
+
+        if (config.email.verbose) console.log(chalk.blueBright("[Email]"), `Email sent to ${this.email.to} (${this.email.subject})`);
+
+        return await this.transporter.sendMail(this.email);
+    };
 }
 
 module.exports = transporter;
