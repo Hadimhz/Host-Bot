c4fe51af5fbb51bbfc2a1428280652830a8bce5b
DarkInk <rstonehouse66@gmail.com>
2021-07-25 13:56:56 -0500

    Updating example-config.json and using user tags

--- commands/Account/user/premium.js
+++ commands/Account/user/premium.js
@@ -19,10 +19,10 @@ module.exports.run = async (client, message, args) => {
     const pp = await userprem.findOne({
         consoleID: userData.consoleID
     });
-    if (!pp) return message.reply('You have No premium servers, You can buy them for <price here> by donating to <linkhere>')
-    if (pp.amount === '0') return message.reply('You have No premium servers, You can buy them for <price here> by donating to <linkhere>')
+    if (!pp) return message.reply(`You have No premium servers, You can buy them for ${config.premiumprice} by donating to ${config.donationlink}`)
+    if (pp.amount === '0') return message.reply(`You have No premium servers, You can buy them for ${config.premiumprice} by donating to ${config.donationlink}`)
     const embed = new MessageEmbed()
-        .setTitle('Premium Servers For ' + `${person.username || person.user.username}`)
+        .setTitle('Premium Servers For ' + `${person.tag || person.user.tag}`)
         .setDescription(`${pp.used} out of ${pp.amount} Servers used`)
         .setColor('BLURPLE')
     message.reply({
