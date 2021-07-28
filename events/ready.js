67b5c72a79aaa1cbb6c315c0a60e077e296ea214
Ritam Choudhuri <xcyth@xcyth.xyz>
2021-07-29 00:51:13 +0530

    improvements

--- events/ready.js
+++ events/ready.js
@@ -2,20 +2,14 @@ const config = require("../config.json");
 const { client, updateCache } = require("../index");
 const mongo = require("../utils/mongo");
 const chalk = require("chalk");
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes();
 
 client.on('ready', async () => {
     console.log(`${chalk.greenBright("[BOT]")} Bot ready and logged in as ${client.user.tag}`);
     await mongo().then(() => console.log(chalk.green("[DATABASE]") + " Connected to database successfully!"))
         .catch(e => console.error(chalk.bgRedBright("[ERROR]"), `An error has occured when attempting to connect to mongo. (${e.message})`));
-    await updateCache().then(() => {
-        console.log(chalk.bgGreen("[CACHE]"), "Updated Cache! Time: " + time)
-    })
+    await updateCache()
     setInterval(async () => {
-        await updateCache().then(() => {
-            console.log(chalk.bgGreen("[CACHE]"), "Updated Cache! Time: " + time)
-        })
+        await updateCache()
     }, 60 * 60 * 1000)
 });
 
