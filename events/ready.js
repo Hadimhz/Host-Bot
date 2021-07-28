725b0225311765ba2048e5da57a8895c82598f4c
Ritam Choudhuri <xcyth@xcyth.xyz>
2021-07-28 11:37:05 +0530

    forgot this

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const config = require("../config.json");
-const { client } = require("../index");
+const { client, updateCache } = require("../index");
 const mongo = require("../utils/mongo");
 const chalk = require("chalk");
 
@@ -7,5 +7,8 @@ client.on('ready', async () => {
     console.log(`${chalk.greenBright("[BOT]")} Bot ready and logged in as ${client.user.tag}`);
     await mongo().then(() => console.log(chalk.green("[DATABASE]") + " Connected to database successfully!"))
         .catch(e => console.error(chalk.bgRedBright("[ERROR]"), `An error has occured when attempting to connect to mongo. (${e.message})`));
+    await updateCache().then(() => {
+        console.log(chalk.greenBright("[UPDATED CACHE]"))
+    })
 });
 
