89ac30e8ce4a5d73ba4151bcf8b602308a1689e7
Hadimhz <solo1010.mahfouz@gmail.com>
2021-07-29 03:22:55 +0300

    [event/ready.js] made it easier to read

--- events/ready.js
+++ events/ready.js
@@ -5,11 +5,13 @@ const chalk = require("chalk");
 
 client.on('ready', async () => {
     console.log(`${chalk.greenBright("[BOT]")} Bot ready and logged in as ${client.user.tag}`);
+
     await mongo().then(() => console.log(chalk.green("[DATABASE]") + " Connected to database successfully!"))
         .catch(e => console.error(chalk.bgRedBright("[ERROR]"), `An error has occured when attempting to connect to mongo. (${e.message})`));
+
+
     await updateCache()
-    setInterval(async () => {
-        await updateCache()
-    }, 60 * 60 * 1000)
+
+    setInterval(async () => await updateCache(), 60 * 60 * 1000)
 });
 
