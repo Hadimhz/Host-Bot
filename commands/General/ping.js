677d89a45b5f44b4752d827fff1e25baade2fbb3
DottoArts <80960175+DottoArts@users.noreply.github.com>
2021-08-01 00:43:25 +0200

    Changes embed color.

--- commands/General/ping.js
+++ commands/General/ping.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
     message.channel.send({
-        embeds: [new MessageEmbed().setTitle("Pong!").setColor("#36393F")
+        embeds: [new MessageEmbed().setTitle("Pong!").setColor("#6f89d9")
         .addField(`> 🖥 API Latency:`, `**${client.ws.ping}ms**`, true)
         .addField(`> 🤖 Bot Latency:`, `**${Date.now() - message.createdTimestamp}ms**`, true)
         .setFooter(`Requested by ${message.author.tag}`).setTimestamp()]
