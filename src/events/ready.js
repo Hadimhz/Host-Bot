ac05a516f70ee82a57c065b7b1702e557eb81040
Ritam Choudhuri <xcyth@xcyth.xyz>
2021-10-19 15:14:39 +0530

    made the logging of members cached more accurate

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,12 +5,14 @@ const chalk = require("chalk");
 
 client.on('ready', async () => {
     const guild = client.guilds.cache.get(config.discord.guild);
-    guild.members.fetch()
     console.log(`${chalk.greenBright("[BOT]")} Bot ready and logged in as ${client.user.tag}`);
-    console.log(`${chalk.greenBright("[BOT]")} Cached All members in ${guild.name}`)
+    
+    guild.members.fetch().then(() => {
+        console.log(`${chalk.greenBright("[BOT]")} Cached All members in ${guild.name}`)
+    })
+
     await mongo().then(() => console.log(chalk.green("[DATABASE]") + " Connected to database successfully!"))
         .catch(e => console.error(chalk.bgRedBright("[ERROR]"), `An error has occured when attempting to connect to mongo. (${e.message})`));
-
 
     await updateCache()
     .catch(e => console.error(chalk.bgRedBright("[ERROR]"), `An error has occured when fetching nodes. (${e.message})`))
