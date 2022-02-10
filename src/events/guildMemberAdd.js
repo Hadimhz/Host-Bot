3e4e867c2ca4ca5d8f71e9ca3159c1da153c4c3a
DarkInk <rstonehouse66@gmail.com>
2022-02-09 23:17:42 -0600

    reworking guildMemberAdd

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -3,8 +3,17 @@ const { client } = require('../index.js');
 const config = require(ROOT_PATH + "/../config.json");
 
 client.on('guildMemberAdd', member => {
-	const wChannel = bot.channels.cache.get(config.channels.welcome);
-	wChannel.send(`<@${member.id}> Welcome`);
-	let joinRole = member.guild.roles.cache.get(config.roles.member);
+
+	if(config.discord.channels.welcome == null) return;
+	if(config.discord.roles.member == null) return;
+
+	const wChannel = client.channels.cache.get(config.discord.channels.welcome);
+	const joinRole = member.guild.roles.cache.get(config.discord.roles.member);
+	const wmsg = config.discord.welcomeMessage ? config.discord.welcomeMessage : "Welcome to the server, {member}!";
+	if(!wChannel) return;
+	if(!joinRole) return;
+
+	wChannel.send(wmsg.replace("{member}", member));
+
 	member.roles.add(joinRole).catch(console.error);
  }); 
